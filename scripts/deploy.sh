a2681970aa9dd7ec0dd65ec85ebc8137b46dfb29
HarishMN <harish.navarathna.hn@gmail.com>
2022-07-29 14:44:39 +0530

    jjgg

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,47 +10,47 @@ else
     KEY="ubuntuking.pem"
 fi
 
+echo $(pwd)
 
+# echo "Making project directory"
 
-echo "Making project directory"
+# timeout 100 ssh -i butterjam.pem ubuntu@52.66.45.141 "mkdir -p /var/www/html/$SERVER"
 
-timeout 100 ssh -i butterjam.pem ubuntu@52.66.45.141 "mkdir -p /var/www/html/$SERVER"
+# echo "Successfully created project directory"
 
-echo "Successfully created project directory"
+# echo "Copying files to remote server"
 
-echo "Copying files to remote server"
+# timeout 100 scp -i butterjam.pem -r build/* ubuntu@52.66.45.141:/var/www/html/$SERVER
 
-timeout 100 scp -i butterjam.pem -r build/* ubuntu@52.66.45.141:/var/www/html/$SERVER
+# echo "Copyed files to remote server"
 
-echo "Copyed files to remote server"
+# echo "Configuring nginx server"
 
-echo "Configuring nginx server"
+# timeout 200 ssh -t -i butterjam.pem ubuntu@52.66.45.141 <<EOF
+# sudo touch /etc/nginx/sites-available/$SERVER
+# sudo touch /var/log/nginx/$SERVER.access.log
+# sudo touch /var/log/nginx/$SERVER.error.log
 
-timeout 200 ssh -t -i butterjam.pem ubuntu@52.66.45.141 <<EOF
-sudo touch /etc/nginx/sites-available/$SERVER
-sudo touch /var/log/nginx/$SERVER.access.log
-sudo touch /var/log/nginx/$SERVER.error.log
+# sudo tee -a /etc/nginx/sites-available/$SERVER <<'eof'
+# access_log      /var/log/nginx/$SERVER.access.log;
+# error_log       /var/log/nginx/$SERVER.access.error.log;
 
-sudo tee -a /etc/nginx/sites-available/$SERVER <<'eof'
-access_log      /var/log/nginx/$SERVER.access.log;
-error_log       /var/log/nginx/$SERVER.access.error.log;
+# server {
+# 	listen			80;
+# 	server_name		$SERVER;
+# 	root			/var/www/html/$SERVER;
+# 	index			index.html index.htm;
+#     location / {
+# 		try_files \$uri /index.html = 404;
+# 		}
+# }
 
-server {
-	listen			80;
-	server_name		$SERVER;
-	root			/var/www/html/$SERVER;
-	index			index.html index.htm;
-    location / {
-		try_files \$uri /index.html = 404;
-		}
-}
+# eof
 
-eof
+# sudo ln -s /etc/nginx/sites-available/$SERVER /etc/nginx/sites-enabled/
+# sudo nginx -t
+# sudo systemctl restart nginx
+# EOF
 
-sudo ln -s /etc/nginx/sites-available/$SERVER /etc/nginx/sites-enabled/
-sudo nginx -t
-sudo systemctl restart nginx
-EOF
-
-echo "Successfully configured nginx server"
-echo "Successfully deployed to $SERVER"
+# echo "Successfully configured nginx server"
+# echo "Successfully deployed to $SERVER"
